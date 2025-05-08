beb0afcb4eb416a83e5d4ceb20d54688a7bc1939
Stanislav Kokoshko <stanislav.kokoshko@whinsygames.co>
2025-05-08 11:07:19 +0300

    Initial Commit

---
+++ QuitSmoking/QuitSmokingApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitSmokingApp.swift
+//  QuitSmoking
+//
+//  Created by Mac on 08.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuitSmokingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuitSmokingTests/QuitSmokingTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuitSmokingTests.swift
+//  QuitSmokingTests
+//
+//  Created by Mac on 08.05.2025.
+//
+
+import Testing
+@testable import QuitSmoking
+
+struct QuitSmokingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
